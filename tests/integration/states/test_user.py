a0e4c09352574b0752b70fd58975cb121f2afe09
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-01-10 11:59:33 +0000

    Return error when gid_from_name and group does not exist.
    Fixes #45345
    
    Ensure empty string gid is set to None
    
    Make pylint happy
    
    Update documentation for 'gid_from_name' parameter
    
    Refactor to prevent logical bug when gid is 0
    
    Fix integration tests for 'user.present' state.

--- tests/integration/states/test_user.py
+++ tests/integration/states/test_user.py
@@ -96,16 +96,43 @@ class UserTest(ModuleCase, SaltReturnAssertsMixin):
         ret = self.run_state('user.present', name=self.user_name,
                              home=HOMEDIR)
         self.assertSaltTrueReturn(ret)
+        ret = self.run_state('user.absent', name=self.user_name)
+        self.assertSaltTrueReturn(ret)
 
-    def test_user_present_nondefault(self):
+
+    @destructiveTest
+    @skipIf(os.geteuid() != 0, 'you must be root to run this test')
+    @requires_system_grains
+    def test_user_present_nondefault(self, grains=None):
         '''
         This is a DESTRUCTIVE TEST it creates a new user on the on the minion.
         '''
-        ret = self.run_state('user.present', name=self.user_name,
-                             home=self.user_home)
+        ret = self.run_state('user.present', name='salt_test',
+                             home='/var/lib/salt_test')
         self.assertSaltTrueReturn(ret)
+        ret = self.run_function('user.info', ['salt_test'])
+        self.assertReturnNonEmptySaltType(ret)
+        group_name = grp.getgrgid(ret['gid']).gr_name
+        if grains['os_family'] in ('Suse',):
+            self.assertEqual(group_name, 'users')
+        elif grains['os_family'] == 'MacOS':
+            self.assertEqual(group_name, 'staff')
+        else:
+            self.assertEqual(group_name, 'salt_test')
+        self.assertTrue(os.path.isdir('/var/lib/salt_test'))
+        ret = self.run_state('user.absent', name='salt_test')
+        self.assertSaltTrueReturn(ret)
+        ret = self.run_function('user.info', [self.user_name])
+        self.assertReturnNonEmptySaltType(ret)
+        group_name = grp.getgrgid(ret['gid']).gr_name
         if not salt.utils.is_darwin():
             self.assertTrue(os.path.isdir(self.user_home))
+        if grains['os_family'] in ('Suse',):
+            self.assertEqual(group_name, 'users')
+        elif grains['os_family'] == 'MacOS':
+            self.assertEqual(group_name, 'staff')
+        else:
+            self.assertEqual(group_name, self.user_name)
 
     @requires_system_grains
     def test_user_present_gid_from_name_default(self, grains=None):
@@ -119,23 +146,26 @@ class UserTest(ModuleCase, SaltReturnAssertsMixin):
         # MacOS users' primary group defaults to staff (20), not the name of
         # user
         gid_from_name = False if grains['os_family'] == 'MacOS' else True
-
-        ret = self.run_state('user.present', name=self.user_name,
+        ret_user_present = self.run_state('user.present', name=self.user_name,
                              gid_from_name=gid_from_name, home=self.user_home)
-        self.assertSaltTrueReturn(ret)
 
-        ret = self.run_function('user.info', [self.user_name])
-        self.assertReturnNonEmptySaltType(ret)
-        group_name = grp.getgrgid(ret['gid']).gr_name
-
-        if not salt.utils.is_darwin():
-            self.assertTrue(os.path.isdir(self.user_home))
-        if grains['os_family'] in ('Suse',):
-            self.assertEqual(group_name, 'users')
-        elif grains['os_family'] == 'MacOS':
-            self.assertEqual(group_name, 'staff')
+        if gid_from_name:
+            self.assertSaltFalseReturn(ret_user_present)
+            ret_user_present = ret_user_present[next(iter(ret_user_present))]
+            self.assertTrue('is not present' in ret_user_present['comment'])
         else:
-            self.assertEqual(group_name, self.user_name)
+            self.assertSaltTrueReturn(ret_user_present)
+            ret_user_info = self.run_function('user.info', [self.user_name])
+            self.assertReturnNonEmptySaltType(ret_user_info)
+            group_name = grp.getgrgid(ret_user_info['gid']).gr_name
+            if not salt.utils.is_darwin():
+                self.assertTrue(os.path.isdir(self.user_home))
+            if grains['os_family'] in ('Suse',):
+                self.assertEqual(group_name, 'users')
+            elif grains['os_family'] == 'MacOS':
+                self.assertEqual(group_name, 'staff')
+            else:
+                self.assertEqual(group_name, self.user_name)
 
     def test_user_present_gid_from_name(self):
         '''
